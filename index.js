84eb3510def1ad9e095c13386cb5aac11b26c6d9
Leo <rb.leonardo@gmail.com>
2015-11-11 01:08:36 -0500

    Integrating parallelshell, ecstatic. Bootstrapping FolderTree component.

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import FolderTree from './src/components/folder-tree'
+
+ReactDOM.render(
+  <FolderTree />, document.querySelector('#content')
+)
---
+++ src/components/folder-tree.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+export default class FolderTree extends Component {
+  render () {
+    return <div>Folder Tree</div>
+  }
+}
